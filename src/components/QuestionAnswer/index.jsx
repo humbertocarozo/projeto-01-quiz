22c03f43ce137721eb949c59e9be44f88551efa5
Humberto Carozo <humbertocarozo2801@gmail.com>
2023-12-19 17:16:23 -0300

    feat: add QuestionAnswer

---
+++ src/components/QuestionAnswer/index.jsx
@@ -0,0 +1,12 @@
+import S from './style.module.css'
+
+export function QuestionAnswer (props) {
+    return (
+        <button
+        className = {S.container}
+        onClick={(event) => props.handleAnswerQuestion(event, props.question, props.answer)}
+        >
+            {props.answer}
+        </button>
+    )
+}
