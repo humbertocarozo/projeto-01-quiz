35fae09257629b188586b61bebb6e5e573db2132
Humberto Carozo <humbertocarozo2801@gmail.com>
2023-12-22 10:47:37 -0300

    feat add: button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,6 @@
+import S from './style.module.css'
+export function Button (props) {
+    return (
+        <button className={S.container} onClick= {props.onClick}>{props.children}</button>
+    )
+}
